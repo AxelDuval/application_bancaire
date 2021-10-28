7313fd3fec2f2e4bf3508973c8306f0c559a0d7c
unknown <axel.duval@gmail.com>
2021-10-28 09:58:36 +0200

    Creating security rules layer with Js

--- js/rules.js
+++ js/rules.js
@@ -1,27 +1,47 @@
-let rules = document.getElementById('rules');
-let rulesBtn = document.getElementById('accept');
-let rulesContent = document.getElementById('rulesContent');
-let rulesTitle = document.getElementById('rulesTitle');
-
-
-
 function hideRules(){
     rules.classList.add('displayNone');
 }
 
-
 window.addEventListener("load", showRules);
 
-
 function showRules(){
+    let rulesBtn = document.createElement('button');
+    rulesBtn.setAttribute("id", "accept");
+    rulesBtn.classList.add("btn", "btn-primary", "text-right");
+
+    let divAccept = document.createElement('div');
+    divAccept.setAttribute("id", "divAccept");
+
+    let rulesContent = document.createElement('p');
+    rulesContent.setAttribute("id", "rulesContent");
+
+    let rulesTitle = document.createElement('h3');
+    rulesTitle.setAttribute("id", "rulesTitle");
+
+    let toRead = document.createElement('div');
+    toRead.setAttribute("id", "toRead");
+
+    let rules = document.createElement('div');
+    rules.setAttribute("id", "rules");
+
+    divAccept.appendChild(rulesBtn);
+    toRead.appendChild(rulesTitle);
+    toRead.appendChild(rulesContent);
+    toRead.appendChild(divAccept);
+
+    rules.appendChild(toRead);
+
+    document.body.appendChild(rules);
+
     fetch('data/security_rules.json').then(function(res){
         return res.json();
     }).then(function(res){
         rulesTitle.innerText = res.title;
-        rulesContent.innerText = res.content;
-        console.log(res);
-        // for (let pokemon of res.results){
-        //    pokeList.innerHTML += `<li>${pokemon.name}</li>`;
-        // }
-    })
+        rulesContent.innerText = res.content; 
+        rulesBtn.innerText = res.button;      
+    });
+
+    rulesBtn.addEventListener('click', function() {
+        rules.classList.add('displayNone');
+    });
 }
