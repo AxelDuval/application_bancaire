be9ee11e4436bdf25ea5b58f1d8657a9160af058
unknown <axel.duval@gmail.com>
2021-12-01 16:10:19 +0100

    Modify structure of th project

---
+++ data/accounts.php
@@ -0,0 +1,50 @@
+
+  
+  <div class="container-fluid">
+
+<div class="row mt-4">
+  <div class="col-xl-3 col-md-6">
+      <div class="card bg-primary text-white mb-4">
+        <div class="card-body">
+          <p class="card-text">Compte courant</p>
+          <p class="card-text">645 €</p></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+              <a class="small text-white stretched-link" href="#">Voir le détail</a>
+              <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+          </div>
+      </div>
+  </div>
+  <div class="col-xl-3 col-md-6">
+      <div class="card bg-warning text-white mb-4">
+        <div class="card-body">
+          <p class="card-text">Livret A</p>
+          <p class="card-text">1000 €</p></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+              <a class="small text-white stretched-link" href="#">Voir le détail</a>
+              <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+          </div>
+      </div>
+  </div>
+  <div class="col-xl-3 col-md-6">
+      <div class="card bg-success text-white mb-4">
+          <div class="card-body">
+            <p class="card-text">PEL</p>
+            <p class="card-text">6000 €</p></div>
+          <div class="card-footer d-flex align-items-center justify-content-between">
+              <a class="small text-white stretched-link" href="#">Voir le détail</a>
+              <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+          </div>
+      </div>
+  </div>
+
+  <div class="col-xl-3 col-md-6">
+    <div class="card bg-info text-white mb-4">
+        <div class="card-body">Créer un nouveau compte </div>
+        <div class="addCount"><a class="small text-white stretched-link" href="creation.html"><i class="fas fa-plus-circle"></i></a></div>
+        
+    </div>
+</div>
+  
+</div>
+
+</div>
---
+++ data/actus.php
@@ -0,0 +1,14 @@
+
+  <main>
+    <div class="container-fluid">
+        <div class="row m-3">
+            <h3>Actualités</h3>
+    </div>
+    
+    <div class="container-fluid">
+        <div id="news" class="row">
+            
+        </div>
+    </div>  
+
+ </main>
