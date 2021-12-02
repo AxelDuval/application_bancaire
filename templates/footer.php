b9fbccb71fb15e7da5c6fced03d2ee4dabfb67e1
AxelDuval <axel.duval@gmail.com>
2021-12-02 11:38:45 +0100

    Show accounts on index.php

--- templates/footer.php
+++ templates/footer.php
@@ -15,7 +15,7 @@
   <script src="js/plugins.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="js/main.js"></script>
-  <script src="js/rules.js"></script>
+  <!-- <script src="js/rules.js"></script> -->
   <script src="js/blog.js"></script>
   <script src="js/creation.js"></script>
   <script src="js/stats.js"></script>
